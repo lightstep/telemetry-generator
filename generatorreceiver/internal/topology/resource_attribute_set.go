bce2fa64d45eb56b509fbfc4cb3a6efbb38c7037
Carolyn Blumberg <carolyn.blumberg@lightstep.com>
2022-09-19 18:35:30 -0400

    change kubernetes to pointer and don't create pods if no kubernetes defined

--- generatorreceiver/internal/topology/resource_attribute_set.go
+++ generatorreceiver/internal/topology/resource_attribute_set.go
@@ -5,8 +5,8 @@ import (
 )
 
 type ResourceAttributeSet struct {
-	Kubernetes          Kubernetes `json:"kubernetes" yaml:"kubernetes"`
-	ResourceAttributes  TagMap     `json:"resourceAttrs,omitempty" yaml:"resourceAttrs,omitempty"`
+	Kubernetes          *Kubernetes `json:"kubernetes" yaml:"kubernetes"`
+	ResourceAttributes  TagMap      `json:"resourceAttrs,omitempty" yaml:"resourceAttrs,omitempty"`
 	EmbeddedWeight      `json:",inline" yaml:",inline"`
 	flags.EmbeddedFlags `json:",inline" yaml:",inline"`
 }
@@ -16,10 +16,10 @@ func (r *ResourceAttributeSet) GetAttributes() *TagMap {
 	for k, v := range r.ResourceAttributes {
 		tm[k] = v
 	}
-
-	for k, v := range r.Kubernetes.GetK8sTags() {
-		tm[k] = v
+	if k8s := r.Kubernetes; k8s != nil {
+		for k, v := range k8s.GetK8sTags() {
+			tm[k] = v
+		}
 	}
-
 	return &tm
 }
---
+++ generatorreceiver/internal/topology/resource_attribute_set_test.go
@@ -0,0 +1,82 @@
+package topology
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestResourceAttributeSet_GetAttributes(t *testing.T) {
+	tests := []struct {
+		name               string
+		service            string
+		resourceAttributes TagMap
+		kubernetes         *Kubernetes
+		expected           TagMap
+	}{
+		{
+			name:    "resource attributes and kubernetes both defined",
+			service: "fake-service",
+			resourceAttributes: map[string]interface{}{
+				"cloud":  "nimbus",
+				"region": "us-central1",
+			},
+			kubernetes: &Kubernetes{
+				ClusterName: "cluster-1",
+				Cfg: &Config{
+					Kubernetes: &KubernetesConfig{
+						PodCount: 1,
+					},
+				},
+			},
+			expected: TagMap{
+				"cloud":               "nimbus",
+				"region":              "us-central1",
+				"k8s.cluster.name":    "cluster-1",
+				"k8s.container.name":  "fake-service",
+				"k8s.namespace.name":  "fake-service",
+				"k8s.deployment.name": "",
+			},
+		},
+		{
+			name:    "resource attributes defined but not kubernetes",
+			service: "some-service",
+			resourceAttributes: map[string]interface{}{
+				"cloud":  "nimbus",
+				"region": "europe-west1",
+			},
+			kubernetes: nil,
+			expected: TagMap{
+				"cloud":  "nimbus",
+				"region": "europe-west1",
+			},
+		},
+		{
+			name:               "neither resource attributes nor kubernetes defined",
+			service:            "another-service",
+			resourceAttributes: map[string]interface{}{},
+			kubernetes:         nil,
+			expected:           TagMap{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resourceAttrSet := ResourceAttributeSet{
+				Kubernetes:         tt.kubernetes,
+				ResourceAttributes: tt.resourceAttributes,
+			}
+
+			if k := resourceAttrSet.Kubernetes; k != nil {
+				rand.Seed(123)
+				k.CreatePods(tt.service)
+
+				// k8s.pod.name structure was copied from CreatePods()
+				rand.Seed(123)
+				tt.expected["k8s.pod.name"] = tt.service + "-" + generateK8sName(10) + "-" + generateK8sName(5)
+			}
+
+			require.Equal(t, tt.expected, *resourceAttrSet.GetAttributes())
+		})
+	}
+}
