f7a1fc2b9c21c3cc0983e49027e0380335f4dd3a
clay.smith <clay.smith@servicenow.com>
2023-05-17 21:32:41 -0700

    support many random tag values

--- generatorreceiver/internal/topology/tag_generator.go
+++ generatorreceiver/internal/topology/tag_generator.go
@@ -3,18 +3,25 @@ package topology
 import "math/rand"
 
 type TagGenerator struct {
-	ValLength int `json:"valLength,omitempty" yaml:"valLength,omitempty"`
-	NumTags   int `json:"numTags,omitempty" yaml:"numTags,omitempty"`
-	NumVals   int `json:"numVals,omitempty" yaml:"numVals,omitempty"`
+	ValLength int    `json:"valLength,omitempty" yaml:"valLength,omitempty"`
+	NumTags   int    `json:"numTags,omitempty" yaml:"numTags,omitempty"`
+	KeyName   string `json:"keyName,omitempty" yaml:"keyName,omitempty"`
+	NumVals   int    `json:"numVals,omitempty" yaml:"numVals,omitempty"`
 	Random    *rand.Rand
 }
 
 func (t *TagGenerator) GenerateTags() map[string]string {
+	retVal := make(map[string]string)
+
+	if len(t.KeyName) != 0 {
+		retVal[t.KeyName] = randStringBytes(t.ValLength, t.Random)
+		return retVal
+	}
+
 	nameGenerator := &TagNameGenerator{
 		random: t.Random,
 	}
 
-	retVal := make(map[string]string)
 	for i := 0; i < t.NumTags; i++ {
 		retVal[nameGenerator.Generate()] = randStringBytes(t.ValLength, t.Random)
 	}
