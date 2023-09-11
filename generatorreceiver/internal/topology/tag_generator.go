89b70409b3b532cde608f91a5221bddba6047515
Jonathan Kart <jkart@lightstep.com>
2023-09-11 08:54:07 -0700

    remove unused code

--- generatorreceiver/internal/topology/tag_generator.go
+++ generatorreceiver/internal/topology/tag_generator.go
@@ -45,13 +45,3 @@ func (t *TagGenerator) GetRefreshedTags() map[string]string{
 
 	return t.tags
 }
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-func randStringBytes(n int, r *rand.Rand) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[r.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
