0fc35144dc82d4ba45f0a95bdbca92e3acbca788
karticus <35704273+karticus@users.noreply.github.com>
2023-09-21 12:38:44 -0400

    Jkart/static tag names (#58)
    ## What is the current behavior?
    The tag generator uses `numVals` to generate random tag names. This
    behavior is preserved.
    
    
    ## What is the new behavior?
    Instead of `numVals` you can specify a list of `tags` effectively making
    the list of tags static. You still rotate the values via `numVals` and
    `valueVariability`
    
    ## Pull request checklist
    
    Please check if your PR fulfills the following requirements:
    
    - [x] Tests(`make test`) for the changes have been added (for bug fixes
    / features) and pass
    - [ ] Docs have been reviewed and added / updated if needed (for bug
    fixes / features)
    - [x] Lint (`make lint`) has passed locally and any fixes were made for
    failures
    
    ## Pull request type
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [x] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## Does this introduce a breaking change?
    
    - [ ] Yes
    - [x] No

--- generatorreceiver/internal/topology/tag_generator.go
+++ generatorreceiver/internal/topology/tag_generator.go
@@ -5,8 +5,12 @@ import (
 )
 
 type TagGenerator struct {
+	// NumTags is used to generate a random tag set if Tags is empty
 	NumTags   int `json:"numTags,omitempty" yaml:"numTags,omitempty"`
 	NumVals   int `json:"numVals,omitempty" yaml:"numVals,omitempty"`
+	// Tags is hardcoded tags for this generator. eg - you may want a couple tags "foo,bar" with 5 values each.
+	// If Tags is non-empty, NumTags is ignored
+	Tags []string `json:"tags,omitempty" yaml:"tags,omitempty"`
 	ValueVariability int `json:"valueVariability,omitempty" yaml:"valueVariability,omitempty"`
 	Random    *rand.Rand
 
@@ -16,7 +20,7 @@ type TagGenerator struct {
 
 func (t *TagGenerator) Init(random *rand.Rand) {
 	t.Random = random
-	t.tags = make(map[string]string, t.NumTags)
+	t.tags = make(map[string]string)
 	t.values = make([]string, t.NumVals)
 
 	rtg := &RandomTagGenerator{random: random}
@@ -24,8 +28,15 @@ func (t *TagGenerator) Init(random *rand.Rand) {
 		t.values[i] = rtg.GenerateValue()
 	}
 
-	for i:=0; i< t.NumTags; i++ {
-		t.tags[rtg.GenerateKey()] = rtg.randomString(t.values)
+	if len(t.Tags) == 0 {
+		t.Tags = make([]string, t.NumTags)
+		for i:=0; i< t.NumTags; i++ {
+			t.Tags[i] = rtg.GenerateKey()
+		}
+	}
+
+	for _, tag :=range t.Tags {
+		t.tags[tag] = rtg.randomString(t.values)
 	}
 }
 
